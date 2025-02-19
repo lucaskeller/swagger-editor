5ca2c697bc9f165d6f760ab3568d8e30f198d09c
Lucas Keller <me@lucaskeller.dev>
2025-02-19 14:09:46 -0300

    chore: fire deploy

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import 'swagger-ui-react/swagger-ui.css';
 
 import SwaggerEditor from './App.jsx';
 
+
 const url = process.env.REACT_APP_DEFINITION_FILE
   ? `${process.env.PUBLIC_URL}${process.env.REACT_APP_DEFINITION_FILE}`
   : process.env.REACT_APP_DEFINITION_URL;
